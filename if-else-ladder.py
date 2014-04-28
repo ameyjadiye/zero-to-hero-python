c3438caadf0a04aa09cd2bc369edd2617cf01605
Amey Jadiye <ameyjadiye@gmail.com>
2014-04-29 00:14:16 +0530

    Amey : if else ladder easy program

---
+++ if-else-ladder.py
@@ -0,0 +1,10 @@
+name = raw_input("Enter your name :");
+
+if name == "Amey":
+	print "Hi Amey , nice to meet you!"
+elif name == "purya":
+	print "Purya ... %$@!&$#"
+elif name == "nitin":
+	print "Nitin bhau _/\_ "
+else:
+	print "I realy dont know who you are! Rajnikant ?"
