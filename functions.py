616d7152ac7508d2353c219e9544f3df8d4d1c8d
Amey Jadiye <ameyjadiye@gmail.com>
2014-04-30 01:01:39 +0530

    Amey : learened functions

---
+++ functions.py
@@ -0,0 +1,11 @@
+def print_my_name(first_name = "Amey",last_name="Jadiye"):
+	print "Hi, i am " + first_name + " " + last_name
+
+print_my_name()
+print_my_name("Mamu")
+print_my_name("Vishal","Purandare")
+
+f_name=raw_input("Enter first name : ")
+l_name=raw_input("Enter last name : ")
+
+print_my_name(f_name,l_name)
