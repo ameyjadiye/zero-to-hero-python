4b83da47e08f374c88d29a57adc73278bfa09900
Amey Jadiye <ameyjadiye@gmail.com>
2014-07-10 08:25:12 +0530

    Dictionary in python same as hashmap in java

---
+++ dict.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+hashmap=dict()
+hashmap['name']='Amey'
+hashmap['surname']='Jadiye'
+hashmap['age']=int(25)
+
+print hashmap
+
+print '\n'
+
+print "Hello ",hashmap['name'],"!!"
